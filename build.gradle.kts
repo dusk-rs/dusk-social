2808a8538390891be213cadcbdccc53d4eaa5697
kiairatech <contact@kiaira.tech>
2020-03-30 12:32:37 -0400

    Network version dependency updated to 0.0.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,7 @@ allprojects {
         //Logging
         implementation("org.slf4j:slf4j-api:1.7.30")
         implementation("ch.qos.logback:logback-classic:1.2.3")
-        implementation("org.redrune.core:redrune-network:0.0.6")
+        implementation("org.redrune.core:redrune-network:0.0.7")
 
         //Utilities
         implementation("com.google.guava:guava:19.0")
